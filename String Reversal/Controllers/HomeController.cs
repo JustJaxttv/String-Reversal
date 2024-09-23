f21659efcc823b4b87bc7dbae1e3da9e86784e10
Kobe Swier <kobe.swier@southeasttech.edu>
2024-09-22 21:33:21 -0500

    Add project files.

---
+++ String Reversal/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using String_Reversal.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace String_Reversal.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(GetString model)
+        {
+            if (ModelState.IsValid) 
+            {
+                model.ReverseString();
+                return View("Result", model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ String Reversal/Models/GetString.cs
@@ -0,0 +1,18 @@
+﻿namespace String_Reversal.Models
+{
+    public class GetString
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+
+        public void ReverseString()
+        {
+            if (!string.IsNullOrEmpty(Input)) 
+            {
+                char[] charArray = Input.ToCharArray();
+                System.Array.Reverse(charArray);
+                Output = new string(charArray);
+            }
+        }
+    }
+}
